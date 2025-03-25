3b353721cd658b412e8bb58c4ce4513a8d8626eb
nandhini-2310 <nandhinibalamurugan2003@gmail.com>
2025-03-25 10:14:40 +0530

    Update db_initialize.go

--- DB/db_initialize.go
+++ DB/db_initialize.go
@@ -1,1 +1,37 @@
+package DB
+
+import (
+	"log"
+
+	"DB_GORM/models"
+	"DB_GORM/utils"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Initialize() {
+	LoadEnv()
+
+	dsn := GetDSN()
+
+	utils.InitLogger()
+	utils.InfoLog.Println("Connecting to DB with DSN:", dsn)
+
+	var err error
+
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	err = DB.AutoMigrate(&models.User{}, &models.Job{}, &models.Recruiter{}, &models.Application{}, &models.Interview{})
+	if err != nil {
+		utils.ErrorLog.Printf("AutoMigrate failed: %v", err)
+	}
+
+	utils.InfoLog.Println("Database connected successfully")
+}
 
